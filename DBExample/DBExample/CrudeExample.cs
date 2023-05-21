1d92987b437f299e0b31c2b8ed1cb222b6f2b3ba
Intaj Tamang <129570575+intajtamang@users.noreply.github.com>
2023-05-21 15:38:41 +0545

    Add files via upload

---
+++ DBExample/DBExample/CrudeExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DBExample
+{
+    class CrudeExample
+    {
+        public void setConnection()
+        {
+            string cs = "Data Source=NCCS-155\\SQLEXPRESS; Initial Catalog=db_csita; Integrated Security=true";
+            try
+            {
+                //Connecting with database
+                SqlConnection conn = new SqlConnection(cs);
+                conn.Open();
+                //checking Connection
+                if(conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+                }
+                //Inserting data into table
+                string inQuery = "insert into tbl_emp values('1','ram','male','IT','manager','50000')";
+                SqlCommand sc = new SqlCommand(inQuery, conn);
+                int res = sc.ExecuteNonQuery();
+                if(res >= 1)
+                {
+                    Console.WriteLine("Data Inserted");
+                }
+            }catch(SqlException s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ DBExample/DBExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBExample
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            CrudeExample c1 = new CrudeExample();
+            c1.setConnection();
+        }
+    }
+}
